ec0c3a3a4e12a7d1bf0ffdbb7da925c52c7fbf0f
Grzegorz Panek <grzegorzpnk@gmail.com>
2026-04-02 18:22:51 +0200

    Initial Commit

---
+++ vibingFlightDiary/vibingFlightDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibingFlightDiaryApp.swift
+//  vibingFlightDiary
+//
+//  Created by Grzegorz Panek on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct vibingFlightDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vibingFlightDiaryTests/vibingFlightDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  vibingFlightDiaryTests.swift
+//  vibingFlightDiaryTests
+//
+//  Created by Grzegorz Panek on 02/04/2026.
+//
+
+import Testing
+@testable import vibingFlightDiary
+
+struct vibingFlightDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
